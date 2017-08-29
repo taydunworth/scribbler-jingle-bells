4822814267864c226cb15f60de17b4aa61f416f6
Taylor Cox <taylorcox@Taylors-MacBook-Pro-2.local>
2017-08-29 19:49:42 -0400

    Added pitch for each note for beep to sing song

---
+++ main.py
@@ -0,0 +1,140 @@
+from Myro import *
+init("/dev/tty.IPRE6-365764-DevB")
+
+setS2Volume(5000)
+
+beep(.5,196)   #g
+beep(.5,330)   #E
+beep(.5,294)   #D
+beep(.5,262)   #C
+beep(.5,196)   #g
+
+wait(1.5)
+
+beep(.25,196)  #g
+beep(.25,196)  #g
+beep(.5,196)   #g
+beep(.5,330)   #E
+beep(.5,294)   #D
+beep(.5,262)   #C
+beep(.5,220)   #a
+
+wait(1.5)
+
+beep(.5,220)   #a
+beep(.5,349)   #F
+beep(.5,330)   #E
+beep(.5,294)   #D
+beep(.5,247)   #b
+
+wait(1.5)
+
+beep(.5,392)   #G
+beep(.5,392)   #G
+beep(.5,349)   #F
+beep(.5,294)   #D
+beep(.5,330)   #E
+
+wait(1.5)
+
+beep(.5,196)   #g
+beep(.5,330)   #E
+beep(.5,294)   #D
+beep(.5,262)   #C
+beep(.5,196)   #g
+
+wait(1.5)
+
+beep(.5,196)   #g
+beep(.5,330)   #E
+beep(.5,294)   #D
+beep(.5,262)   #C
+beep(.5,220)   #a
+
+wait(1.5)
+
+beep(.5,220)   #a
+beep(.5,349)   #F
+beep(.5,330)   #E
+beep(.5,294)   #D
+beep(.5,392)   #G
+beep(.5,392)   #G
+beep(.5,392)   #G
+beep(.5,392)   #G
+beep(.5,440)   #A
+beep(.5,392)   #G
+beep(.5,349)   #F
+beep(.5,294)   #D
+beep(.5,262)   #C
+
+wait(1.5)
+
+
+#chorus
+
+beep(.5,330)   #E
+beep(.5,330)   #E
+beep(.5,330)   #E
+wait(.5)
+beep(.5,330)   #E
+beep(.5,330)   #E
+beep(.5,330)   #E
+wait(.5)
+beep(.5,330)   #E
+beep(.5,392)   #G
+beep(.5,262)   #C
+beep(.5,294)   #D
+beep(.5,330)   #E
+
+wait(1.5)
+
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,330)   #E
+beep(.5,330)   #E
+beep(.25,330)  #E
+beep(.25,330)  #E
+beep(.25,330)  #E
+wait(.2)
+beep(.5,294)   #D
+beep(.5,294)   #D
+beep(.5,330)   #E
+beep(.5,294)   #D
+wait(.5)
+beep(.5,392)   #G
+
+wait(.5)
+
+beep(.5,330)   #E
+beep(.5,330)   #E
+beep(.5,330)   #E
+wait(.5)
+beep(.5,330)   #E
+beep(.5,330)   #E
+beep(.5,330)   #E
+wait(.5)
+beep(.5,330)   #E
+beep(.5,392)   #G
+beep(.5,262)   #C
+beep(.5,294)   #D
+beep(.5,330)   #E
+
+wait(1.5)
+
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,349)   #F
+beep(.5,330)   #E
+beep(.5,330)   #E
+beep(.25,330)  #E
+beep(.25,330)  #E
+beep(.5,392)   #G
+beep(.5,392)   #G
+beep(.5,349)   #F
+beep(.5,294)   #D
+beep(.5,262)   #C
